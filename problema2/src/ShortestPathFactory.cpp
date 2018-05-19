e9a536cf1e0ab86f4b90a046c1377a5bf73f5608
Pablo Salinas <pablo.salinas@avature.net>
2018-05-19 11:04:48 -0300

    Agrego la logica para devolver un objeto de tipo Dantzig

--- problema2/src/ShortestPathFactory.cpp
+++ problema2/src/ShortestPathFactory.cpp
@@ -1,9 +1,13 @@
 #include "ShortestPathFactory.h"
+#include "Dantzig.h"
 
 /**
  * Devuelve el algoritmo para resolver camino minimo
  * correspondiente al nombre pasado por parametro
  */
 ShortestPath* ShortestPathFactory::crearAlgoritmo(std::string nombre) {
+    if (nombre == "dantzig") {
+        return new Dantzig();
+    }
     return NULL;
 }
