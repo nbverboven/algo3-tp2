1dac2de19f1e02f5cf32dba944a6966fec01fd90
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-05-20 18:26:03 -0300

    Revert "Teniendo la matriz de distancias no hace falta tener el peso de las aristas en la lista de sucesores. Renombro variable para reflejar el pseudocódigo del informe"
    This reverts commit 56c665b9afe2dc04d5c26516d1e578f320c3a20d.

--- problema2/src/Dijkstra.h
+++ problema2/src/Dijkstra.h
@@ -21,18 +21,16 @@ public:
      * de camino minimo. Las clases que implementen esta interfaz
      * deben implementar este metodo.
      */
-    virtual std::vector<std::vector<double>> resolver(std::vector<Ruta> &rutas, std::vector<int> costos, int n);
-
+    virtual std::vector<std::vector<double>> resolver(std::vector<Ruta>& rutas, std::vector<int> costos, int n);
 private:
-    
     /**
      * Funcion auxiliar que corre Dijsktra sobre un vertice. Se usa para
      * correr Dijkstra secuencialmente sobre todos los vertices del grafo.
      */
-    void DijkstraAux(std::vector<double> &min_path_len,
+    void DijkstraAux(std::vector<double>& min_path_len,
         int vertex,
-        std::vector<std::vector<double>> &distancias,
-        std::vector<std::vector<int>> &adjacencyList);
+        std::vector<std::vector<double>>& distancias,
+        std::vector<std::vector<std::tuple<int, int>>>& adjacencyList);
 };
 
 #endif
